7be640a2b781807ae84866a15a1bdde3549135e4
Darrel O'Pry <darrel.opry@spry-group.com>
2018-07-25 16:48:00 -0400

    chore: implement tests with cordova-paramedic

---
+++ tests/tests.js
@@ -0,0 +1,9 @@
+/* jshint jasmine: true */
+
+export function defineAutoTests () {
+    describe("BeaconRegion Tests", () => {
+        it('is defined', function() {
+            expect(BeaconRegion).toBeDefined();
+        });
+    });
+}
